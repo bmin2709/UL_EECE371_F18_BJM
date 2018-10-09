9c2719c1fe6b60ab60d9d6fa37ceda859448cb75
Bobby Miniex <bmin2709@gmail.com>
2018-10-09 14:30:17 -0500

    sorry i thought id pushed it up last class

---
+++ verilog.v
@@ -0,0 +1,9 @@
+module awful(a, b, c, cout, acc); 
+	input[7:0] a; 
+	input[7:0] b; 
+	input[7:0] acc; 
+	output[15:0] cout; 
+	output[15:0] c; 
+	assign cout = a*b; 
+	assign c = acc +cout; 
+endmodule
